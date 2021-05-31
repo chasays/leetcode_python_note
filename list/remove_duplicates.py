273ec84083e25c99ada3ccaab7b8fa1d93c81e94
叉叉敌 <ievjai@126.com>
2021-05-31 17:54:57 +0800

    Update remove_duplicates.py

--- list/remove_duplicates.py
+++ list/remove_duplicates.py
@@ -1,12 +1,16 @@
-```md
+"""
+给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+给的nums是一个升序排列好的数组。
+
+"""
 
 
-```
+# Resolution
 
 
-Resolution
-
-```py
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
@@ -21,4 +25,3 @@ class Solution:
         
         return slow+1
 
-```
