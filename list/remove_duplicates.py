42c7cafb58ab42d1e6029f7951a07aa24695243b
叉叉敌 <ievjai@126.com>
2021-06-01 21:19:59 +0800

    Update remove_duplicates.py

--- list/remove_duplicates.py
+++ list/remove_duplicates.py
@@ -14,14 +14,12 @@
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not len(nums) : return 
+        if not nums: return 0
         slow = fast = 0
         while fast < len(nums):
-            if nums[slow] == nums[fast]:
-                fast += 1
-            else:
+            if nums[slow] != nums[fast]:
                 slow += 1
-                nums[slow] = nums[fast]  
-        
+                nums[slow] = nums[fast]
+            fast += 1
         return slow+1
 
